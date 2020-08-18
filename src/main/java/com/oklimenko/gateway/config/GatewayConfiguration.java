f642056e6344baafd3ed74470917bd37d138935e
Oleg Klimenko <oklimenko@gmail.com>
2020-08-18 10:29:47 +0300

    Processing route added

--- src/main/java/com/oklimenko/gateway/config/GatewayConfiguration.java
+++ src/main/java/com/oklimenko/gateway/config/GatewayConfiguration.java
@@ -11,7 +11,7 @@ public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests().
-            antMatchers("/oauth/**", "/user/register").//, "/payment/**").
+            antMatchers("/oauth/**", "/user/register", "/processing/**").//, "/payment/**").
             permitAll().
             antMatchers("/**").
             authenticated();
